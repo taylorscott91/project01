463d9ef7bf49e99ff6e4b96ec12e576a6d4bfe48
Taylor Scott <TaylorScott@Taylors-MacBook-Pro.local>
2017-02-01 09:14:46 -0500

    copied over finished work

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # project01
 Project 01 - Trivia
+
+Installation: fork+clone the repo, then run index.html! Easy.
+
+Bronze goal: Basic setup in place, click events working, basic functionality in place. (MVP)
+
+Silver goal: Penalty system actually working
+
+Gold goal: Animations, radio button system as opposed to input?
+
+
+As a player, I should be able to input my answers and see if I'm correct!
+As a player, I should be able to easily track my score to see how well I'm doing.
+As a player, I should be able to show my knowledge of video game trivia.
+As a player, I should be able to compete with my friends to see who knows the most about random video game trivia.
+As an aspiring coder, I can examine the code to better understand the syntax and usage of JS/JQuery.
+
+Process:
+
+Writing questions and setting up variables/event listeners was the first step, and that step is pretty easy. Setting up the questions as an array means the function to pull up questions can pull them up in sequence easily. For scoring, I just created a counter and a function to add 1 to the counter when a question is answered right. Unfortunately, I can't seem to figure out how to implement some kind of penalty system. I thought it would be the same as score adding, just using subtraction.... but I've tried a few methods and nothing seems to be working. Alas.
+
+Things to Work On:
+Fixing this penalty system, for one. Also, figuring out how to hide the "Correct!" message when you pull up a new question.
--- script.js
+++ script.js
@@ -1,1 +1,75 @@
-console.log("Hello")
+console.log("Hello, welcome to trivia.")
+
+var questions = [
+  {
+    question: "Complete this Pac-Man related series: Blinky, Pinky, Inky, and _______. <br><br> A. Clyde <br> B. Winky <br> C. Dinky <br> D. Wakka Wakka Wakka",
+    answer: "a"
+  }, {
+    question: "What game was the debut of everyone's favorite Italian plumber, Mario?<br><br>A. Mario Bros<br>B. Donkey Kong<br>C. Super Mario Bros<br>D. Tetris",
+    answer: "b"
+  }, {
+    question: "What game, due to a massive glitch, has influenced real-world practices on how to control disease outbreaks?<br><br>A. Plants vs Zombies<br>B. Resident Evil<br>C. World of Warcraft<br>D. Madden NFL 2006",
+    answer: "c"
+  }, {
+    question: "Which of these is not a Pokemon?<br><br>A. Klingklang<br>B. Alakazam<br>C. Kabuterimon<br>D. Lickillicky ",
+    answer: "c"
+  }, {
+    question: "Which is not a piece of the Triforce?<br><br>A. Power<br>B. Strength<br>C. Courage<br>D. Wisdom",
+    answer: "b"
+  }, {
+    question: "In the classic football game Tecmo Bowl from 1987, what player was notorious for being much better than every other character in the game?<br><br>A. Bo Jackson<br>B. John Elway<br>C. Jerry Rice<br>D. Tom Brady",
+    answer: "a"
+  }, {
+    question: "In the Resident Evil series, what virus was responsible for zombifying the Earth's population?<br><br>A. X-virus<br>B. T-virus<br>C. Z-virus<br>D. swine flu",
+    answer: "b"
+  }, {
+    question: "Which character is not a character in the Street Fighter series?<br><br>A. El Fuerte<br>B. Skullomania<br>C. Q<br>D. Kung Lao",
+    answer: "d"
+  }, {
+    question: "What is the current world record for fastest completion time of the game Super Mario World?<br><br>A. 78h 34m 12s<br>B. 0h 58m 27s<br>C. 2h 45m 56s<br>D. 0h 1m 15s",
+    answer: "d"
+  }, {
+    question: "What is the real name of the Master Chief from the Halo series?<br><br>A. Steve<br>B. Roger<br>C. John<br>D. Bartholomew",
+    answer: "c"
+  }
+]
+
+var counter = 0;
+var score = 0;
+var input = $(".answer").val();
+
+$(document).ready (function () {
+
+  $(".questionbutton").on("click", showQuestion);
+    function showQuestion () {
+      $(".question").html(questions[counter].question);
+      $(".answer").val("");
+      // $(".comments").html("");
+    }
+
+  $(".submit").on("click", scoreThis);
+  function scoreThis () {
+    if ($(".answer").val() == questions[counter].answer) {
+      $(".text").html("Correct! Click Question to continue.");
+      counter++;
+      score += 1;
+      $(".score").html("Score: " + score);
+      $(".answer").val("");
+          }
+    else {
+      $(".score").html("Score: " + score);
+      $(".text").html("Try again!");
+    //   counter++
+    //   if (penalty == 2) {
+    //    score -= 1;
+    //    penalty = 0
+    //  }
+  }
+  if (score >= 10) {
+    $(".perfect").html("Wow! You know a lot about games! Probably too much. Go read a book.");
+    }
+  // else if (score < 10) {
+  //   $(".perfect").html("It's probably okay that you don't know all the answers. You're still a champ!")
+  //   }
+  }
+})
